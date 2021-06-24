0b47117b64b2ce0ac6471cb9c0457ac9dfc0c8f0
Quangdao Nguyen <quangdaon@gmail.com>
2021-06-24 14:52:04 -0500

    Injection Key

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,1 +1,4 @@
-export const SignalRSymbol = Symbol();
+import { SignalRService } from "./service";
+import { InjectionKey } from "vue";
+
+export const SignalRSymbol: InjectionKey<SignalRService> = Symbol('SignalRService');
