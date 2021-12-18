a46c78886479ae20602c72e7f9f098fbad848657
Quangdao Nguyen <me@quangdao.com>
2021-12-18 15:48:14 -0600

    Documentation

--- src/tokens.ts
+++ src/tokens.ts
@@ -1,2 +1,5 @@
-export interface HubCommandToken<T> extends String {}
+/** The name of a command to send to the SignalR server */
+export interface HubCommandToken<T> extends String { }
+
+/** The name of an event sent by the SignalR server */
 export interface HubEventToken<T> extends String {}
